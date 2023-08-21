cd8d3e9b56446e91951b222c2752632e4c33af3d
Noto <notosgnikusnsi@gmail.com>
2023-08-21 15:16:16 +0900

    Add: 日記の追加機能の実装

--- public/script.js
+++ public/script.js
@@ -3,11 +3,30 @@
 window.onload = async () => {
   const response = await fetch("get_diary");
   const json = await response?.json();
-  if (json && json["diaries"] !== undefined) {
-    for (const diary of json["diaries"]) {
-      document.getElementById("diary-list").innerHTML +=
+  if (json && json["diary"] !== undefined) {
+    for (const diary of json["diary"]) {
+      document.getElementById("diary-list").innerHTML += await
       `
-      <tr class="id" id="id"><td id="date">${date}</td><td class="weather" id="weather"><img class="img" src="${weather}.png" alt="weather"/></td><td class="text" id="text">${text}</td></tr>`
+      <tr class="id" id="id"><td id="date">${diary[date]}</td><td class="weather" id="weather"><img class="img" src="${diary[weather]}.png" alt="weather"/></td><td class="text" id="text">${diary[text]}</td></tr>`
     }
   }
 }
+
+// 日記の追加
+document.getElementById("diary-button").onclick = async() => {
+
+  const text = document.getElementById("text-input").value;
+  const date = document.getElementById("date-input").value;
+  const weather = document.getElementById("weather-input").value;
+  const response = await fetch("/insert-diary",{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      date: date,
+      weather: weather,
+      text: text,
+    })
+  });
+  document.getElementById("diary-list").innerHTML = await `
+  <tr class="id" id="id"><td id="date">${diary[date]}</td><td class="weather" id="weather"><img class="img" src="${diary[weather]}.png" alt="weather"/></td><td class="text" id="text">${diary[text]}</td></tr>`;
+};

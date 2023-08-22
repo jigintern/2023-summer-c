454073c98b2d90308065742af5d8642a4ef31fc7
Noto <notosgnikusnsi@gmail.com>
2023-08-22 10:06:32 +0900

    feat: 日記自動生成のフロントエンド実装

--- public/script.js
+++ public/script.js
@@ -28,3 +28,16 @@ document.getElementById("diary-button").onclick = async() => {
   });
   document.getElementById("diary-list").innerHTML = await `<tr class="id" id="id"><td id="date">${diary[date]}</td><td class="weather" id="weather"><img class="img" src="${diary[weather]}.png" alt="weather"/></td><td class="text" id="text">${diary[text]}</td></tr>`;
 };
+
+// 日記の自動生成
+document.getElementById("gpt-button").onclick = async () => {
+  const words = document.getElementById("gpt-input").value;
+  const response = await fetch("/gpt",{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      words: words
+    })
+  });
+  document.getElementById("text-input").innerText = await response.text();
+}
